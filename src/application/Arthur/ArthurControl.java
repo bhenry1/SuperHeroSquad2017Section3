dd0c5b56c2efc077c432b4e7dedf54c89b875680
bhenry1 <bhenry2@ggc.edu>
2017-11-02 18:47:57 -0400

    Finished map for first Dungeon and placed monsters in Morester

---
+++ src/application/Arthur/ArthurControl.java
@@ -0,0 +1,85 @@
+package application.Arthur;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class ArthurControl 
+{
+	ArthurModel arm = new ArthurModel();
+
+    @FXML
+    private Button dungeonbtn;
+
+    @FXML
+    private Button tavernBtn;
+
+    @FXML
+    private Label cityLabel;
+
+    @FXML
+    private Button shopButton;
+
+    @FXML
+    private Button menuBtn;
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private Button backToWMbtn;
+
+    @FXML
+    private Button puzzleBtn;
+
+    @FXML
+    private ImageView Morestet;
+
+    @FXML
+    private Button innBtn;
+
+    @FXML
+    void returnToWM(ActionEvent event) throws IOException 
+    {
+    	arm.rtnToWorldMap(event);
+
+    }
+
+    @FXML
+    void openMenu(ActionEvent event) 
+    {
+    	
+    	
+    }
+
+    @FXML
+    void enterDungeon(ActionEvent event) {
+
+    }
+
+    @FXML
+    void startPuzzle(ActionEvent event) {
+
+    }
+
+    @FXML
+    void enterTavern(ActionEvent event) {
+
+    }
+
+    @FXML
+    void enterShop(ActionEvent event) {
+
+    }
+
+    @FXML
+    void enterInn(ActionEvent event) {
+
+    }
+
+}
