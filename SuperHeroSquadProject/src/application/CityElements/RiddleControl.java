c0232633a5328af8b76df495792c777df7a12bc5
patrickpage <ppage@ggc.edu>
2017-11-13 10:27:21 -0500

    added reading riddles from text.. still in work

--- SuperHeroSquadProject/src/application/CityElements/RiddleControl.java
+++ SuperHeroSquadProject/src/application/CityElements/RiddleControl.java
@@ -1,14 +1,19 @@
 package application.CityElements;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
-public class RiddleControl {
+public class RiddleControl implements Initializable {
 
     @FXML
     private TextField riddleTextfield;
@@ -23,14 +28,30 @@ public class RiddleControl {
     private Button checkButton;
 
     @FXML
-    private Label riddleLabel;
+    private TextArea riddleTextField;
 
     @FXML
     private ImageView scrollImage;
-
+    
+    RiddleModel rm = new RiddleModel();
+    
     @FXML
-    void validateAnswer(ActionEvent event) {
+    void validateAnswer(ActionEvent event) 
+    {
 
     }
+    
+    
+	@Override
+	public void initialize(URL location, ResourceBundle resources) 
+	{
+		rm.openRiddleText();
+		String riddle = rm.readPuzzle();
+		rm.closeFile();
+		
+		riddleTextField.setText(riddle);
+	}
+    
+    
 
 }
---
+++ SuperHeroSquadProject/src/application/CityElements/RiddleModel.java
@@ -0,0 +1,39 @@
+package application.CityElements;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class RiddleModel 
+{
+	private Scanner rInput;
+	
+	
+	public void openRiddleText()
+	{
+		try {
+			rInput = new Scanner(new File("TextFiles/Riddles.txt"));
+		} 
+			catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public String readPuzzle()
+	{
+		while(rInput.hasNext())
+		{
+			String clourtavRiddle = rInput.nextLine();
+			
+			return clourtavRiddle;
+		}
+		
+		return "";
+	}
+	
+	public void closeFile()
+	{
+		rInput.close();
+	}
+}
